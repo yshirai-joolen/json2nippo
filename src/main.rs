a2d3596b6cbbdba66f1c814e57691d15e3c1d318
yshirai <yshirai@joolen.com>
2023-04-11 21:44:41 +0900

    コマンドライン引数を使えるようにする

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-// use std::env;
+use std::env;
+
 extern crate mknippo;
 
 fn main() {
-    let filename = "test_nippo.txt";
-    mknippo::run(filename);
+    let args: Vec<String> = env::args().collect();
+    let nippo_type = &args[1];
+    let filename = format!("test_nippo_{}.txt", nippo_type);
+    println!("{}", filename);
+    // let filename = "post_test.txt";
+    mknippo::run(&filename);
 }
 
